7a957ab9d28ebb574554e82ff7090beb3203ceed
Jovica Bumbulovic <jovica.bumbulovic@knowit.se>
2026-05-07 10:33:08 +0200

    Show event end date in archive cards
    Render events as a date range in card meta, using start_datum and slut_datum with single-day and multi-day formatting rules.

---
+++ wp-content/mu-plugins/sater-events-filtering/views/v3/Card/components/date.blade.php
@@ -0,0 +1,39 @@
+@php
+    $endTs = null;
+    if (is_array($date) && isset($date['endTimestamp']) && is_numeric($date['endTimestamp'])) {
+        $endTs = (int) $date['endTimestamp'];
+    }
+    $startTs = null;
+    if (is_array($date) && isset($date['timestamp']) && is_numeric($date['timestamp'])) {
+        $startTs = (int) $date['timestamp'];
+    }
+@endphp
+
+@typography(['variant' => 'meta', 'element' => 'span', 'classList' => [$baseClass . '__date']])
+    @icon(['icon' => 'date_range', 'size' => 'sm'])
+    @endicon
+
+    @if ($startTs && $endTs)
+        @php
+            $sameDay = wp_date('Y-m-d', $startTs) === wp_date('Y-m-d', $endTs);
+
+            // Requested rules:
+            // - Single day: "6 maj 2026 13.00-16.00"
+            // - Multi day:  "6 maj 2026 13.00 - 8 maj 2026 14.00"
+            $dateOnly  = wp_date('j M Y', $startTs);
+            $startTime = wp_date('H.i', $startTs);
+            $endTime   = wp_date('H.i', $endTs);
+
+            $formatted = $sameDay
+                ? ($dateOnly . ' ' . $startTime . '-' . $endTime)
+                : (wp_date('j M Y H.i', $startTs) . ' - ' . wp_date('j M Y H.i', $endTs));
+        @endphp
+
+        {{ $formatted }}
+    @else
+        {{-- Default behavior for non-events or if endTimestamp is missing --}}
+        @date($date)
+        @enddate
+    @endif
+@endtypography
+
